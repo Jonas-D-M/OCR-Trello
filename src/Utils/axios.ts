12af2d01a0a68e39aa952abb20c88cfecc11a9be
JonasDM <jonasdemeyer@outlook.com>
2021-04-13 15:13:10 +0200

    use store in axios request

--- src/Utils/axios.ts
+++ src/Utils/axios.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 import Environment from "../config/environment";
-import localStorage from "./localStorage";
+import { store } from "../Redux/store";
 
 const AxiosInstance = axios.create({
   baseURL: " https://api.trello.com/1",
 });
 
 AxiosInstance.interceptors.request.use(async (config) => {
-  config.params["token"] = await localStorage.read("@Token");
+  const state = store.getState();
+  config.params["token"] = state.user.token;
   config.params["key"] = Environment["TRELLO_API_KEY"];
   return config;
 });
