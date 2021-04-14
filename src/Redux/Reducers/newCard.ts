8d5571b8c2e13796f6038cfbd9c9362ae842e4dd
JonasDM <jonasdemeyer@outlook.com>
2021-04-14 12:08:21 +0200

    add state logic to add description for a new card

--- src/Redux/Reducers/newCard.ts
+++ src/Redux/Reducers/newCard.ts
@@ -1,20 +1,49 @@
-export const ADD_NEW_CARD = "ADD_NEW_CARD";
+export const ADD_CARD_DESCRIPTION = "ADD_CARD_DESCRIPTION";
+export const ADD_TITLES_TO_CARDS = "ADD_TITLES";
 export const DELETE_STORED_CARDS = "DELETE_STORED_CARDS";
+export const SET_LIST_ID = "SET_LIST_ID";
 
-const initialState = {};
+const initialState = {
+  listId: "",
+  cards: [],
+};
 
 const newCardsReducer = (state = initialState, action: any) => {
   switch (action.type) {
-    case ADD_NEW_CARD:
-      const key = action.payload.listId;
-      return {
-        ...state,
-        [key]: [
+    case ADD_CARD_DESCRIPTION:
+      //@ts-ignore
+      if (state.cards.some((card) => card.title === action.payload.title)) {
+        //card is in array
+        const tempArr = state.cards.map((card) =>
           //@ts-ignore
-          ...state[key],
-          { title: action.payload.title, desc: action.payload.desc },
-        ],
-      };
+          card.title === action.payload.title
+            ? //@ts-ignore
+              { ...card, desc: action.payload.desc }
+            : card
+        );
+        return {
+          ...state,
+          //@ts-ignore
+          cards: tempArr,
+        };
+      } else {
+        return {
+          ...state,
+        };
+      }
+
+    case SET_LIST_ID:
+      return { ...state, listId: action.payload };
+
+    case ADD_TITLES_TO_CARDS:
+      const titles: Array<string> = action.payload;
+
+      const tempCards = titles.map((title: string) => ({
+        title,
+        listId: state.listId,
+        desc: "",
+      }));
+      return { ...state, cards: tempCards };
     case DELETE_STORED_CARDS:
       return initialState;
     default:
